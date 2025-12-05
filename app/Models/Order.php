51dc29c202f2e31e442e2bb39e9009546bac886b
Davit Gabashvili <datigabashvili@gmail.com>
2025-12-05 14:38:37 +0400

    add relationships in models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
-    //
+
+    public function orderedItems(): HasMany
+    {
+        return $this->hasMany(OrderedItem::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/OrderedItem.php
+++ app/Models/OrderedItem.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class OrderedItem extends Model
 {
-    //
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function design(): BelongsTo
+    {
+        return $this->belongsTo(Design::class);
+    }
+
+    public function phoneModel(): BelongsTo
+    {
+        return $this->belongsTo(PhoneModel::class);
+    }
 }
